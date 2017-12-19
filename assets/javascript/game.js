6d35da0cfab9eca4d37a6c9ce57ac4b5c8038d06
Tom LaChance <guitarlanguageseries@gmail.com>
2017-12-19 16:50:01 -0600

    added images, background, put the javascript in a file seperate from the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,77 @@
 //computer creates a random number between 19 and 120
-// global variable to include in future functions
-var computersRandomlyGeneratedNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-console.log(computersRandomlyGeneratedNumber);
-//when the user clicks a button, a number is generated between 1 and 12
-//(assign each button a number randomly)
-var possibleButtonAdderValues = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-console.log(possibleButtonAdderValues);
-// assign a variable to each button within the scope to keep it a 5 for example throughout the game
-// when guesses match random number you win, when it doesn't they lose
+var computersRandomlyGeneratedNumber = Math.floor(Math.random() * 102) + 19;
+// creating random values between 1 and 12 for each ruby(chrystal) #ID
+var randomRedRubyValue = Math.floor(Math.random() * 12) + 1;
+var randomBlueRubyValue = Math.floor(Math.random() * 12) + 1;
+var randomYellowRubyValue = Math.floor(Math.random() * 12) + 1;
+var randomGreenRubyValue = Math.floor(Math.random() * 12) + 1;
+
+// counter, wins, losses
 var wins = 0;
 var losses = 0;
+var totalScore;
+var counter;
 
-// var crystal1 =
-  // var crystal2 =
-  // var crystal3 =
-  // var crystal4 =
-  // var yourScore =
+//this function will reset (the computer's number to match )and the (user's score)
+function resetComputersRandomNumbertoMatchandyourTotalScoreIsThis(){
+  console.log("resetComputersRandomNumbertoMatchandyourTotalScoreIsThis");
+  totalScore = 0;
+  counter = 0;
+  computersRandomlyGeneratedNumber = Math.floor(Math.random() * 102) + 19;
+  $("#yourTotalScoreIsThis").text(totalScore);
+  $(".insert").text(computersRandomlyGeneratedNumber);
+}
+//binding function to include all ruby click events
+function binding(){
+  console.log("binding");
+  $("#blueRuby").on("click", function() {
+      counter += randomBlueRubyValue;
+      $("#yourTotalScoreIsThis").html(" " + counter);
+      checkWinOrLose();
+  });
 
-//when game ends the random computer number is reset and a new computer number is generated
+  $("#yellowRuby").on("click", function() {
+      counter += randomYellowRubyValue;
+      $("#yourTotalScoreIsThis").html(" " + counter);
+      checkWinOrLose();
+  });
+
+  $("#redRuby").on("click", function() {
+      counter += randomRedRubyValue;
+      $("#yourTotalScoreIsThis").html(" " + counter);
+      checkWinOrLose();
+  });
+
+  $("#greenRuby").on("click", function() {
+      counter += randomGreenRubyValue;
+      $("#yourTotalScoreIsThis").html(" " + counter);
+      checkWinOrLose();
+  });
+}
+//function to check if the goal of the game has been met
+function checkWinOrLose(){
+    console.log("checkWinOrLose");
+    if(counter < computersRandomlyGeneratedNumber){
+    return;
+  }
+  else if(counter === computersRandomlyGeneratedNumber){
+    wins++;
+    $(".winsCounter").html("wins: " + wins);
+    alert("you win");
+    resetComputersRandomNumbertoMatchandyourTotalScoreIsThis();
+  } else if(counter >= computersRandomlyGeneratedNumber){
+    losses++;
+    $(".lossCounter").html("losses: " + losses);
+    alert("you lose");
+    resetComputersRandomNumbertoMatchandyourTotalScoreIsThis();
+  }
+}
+
+//rules: when game ends the random computer number is reset and a new computer number is generated
 //new numbers are assigned to the buttons.
-
-
-
 $(document).ready(function() {
-  $("#computersRandomNumbertoMatch").text(computersRandomlyGeneratedNumber);
-
+    //setting up the game
+    resetComputersRandomNumbertoMatchandyourTotalScoreIsThis();
+    //adding accumulated click events
+    binding();
 });
